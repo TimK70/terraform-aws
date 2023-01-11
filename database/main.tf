e983d1bebcd07f2d37d9008a37f1eb629f8f96ea
TimK70 <tkrass1@gmail.com>
2023-01-11 22:50:33 +0000

    Worked on EC2, SSH & Userdata

--- database/main.tf
+++ database/main.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "two_tier_db" {
   db_subnet_group_name   = var.db_subnet_group_name
   vpc_security_group_ids = var.vpc_security_group_ids
   identifier             = var.db_identifier
-  skip_final_snapshot    = var.skip_db_snapshot
+  skip_final_snapshot    = true
   tags = {
     Name = "two_tier_db"
   }
--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -8,7 +8,11 @@ resource "aws_lb" "two_tier_lb" {
 }
 
 resource "aws_lb_target_group" "two_tier_tg" {
-  name     = "two-tier-tg-${substr(uuid(), 0, 3)}"
+  name = "two-tier-tg-${substr(uuid(), 0, 3)}"
+  lifecycle {
+    ignore_changes        = [name]
+    create_before_destroy = true
+  }
   port     = var.tg_port     #80
   protocol = var.tg_protocol # "HTTP"
   vpc_id   = var.vpc_id
@@ -21,12 +25,12 @@ resource "aws_lb_target_group" "two_tier_tg" {
 }
 
 resource "aws_lb_listener" "two_tier_lb_listener" {
-         load_balancer_arn = aws_lb.two_tier_lb.arn
-         port = var.listener_port #80
-         protocol = var.listener_protocol #"HTTP"
-         default_action {
-            type = "forward"
-            target_group_arn = aws_lb_target_group.two_tier_tg.arn      
-         }
-         
+  load_balancer_arn = aws_lb.two_tier_lb.arn
+  port              = var.listener_port     #80
+  protocol          = var.listener_protocol #"HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.two_tier_tg.arn
+  }
+
 }
