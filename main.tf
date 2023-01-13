c921d8c38bc54357f14286acdd8c88e2dd965071
TimK70 <tkrass1@gmail.com>
2023-01-13 04:17:36 +0000

    Updated work on MTC coursese

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "networking" {
   access_ip        = var.access_ip
   security_groups  = local.security_groups
   public_sn_count  = 2
-  private_sn_count = 3
+  private_sn_count = 4
   max_subnets      = 20
   public_cidrs     = [for i in range(2, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
   private_cidrs    = [for i in range(1, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
